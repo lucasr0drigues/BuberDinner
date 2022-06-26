ce03e958a96284ec1aaeba61d300b27a7c424fda
lucasr0drigues <lucas.a.rodrigues@outlook.com>
2022-06-26 18:57:35 -0300

    lesson 4

--- BuberDinner.Api/Program.cs
+++ BuberDinner.Api/Program.cs
@@ -1,16 +1,21 @@
 
+using BuberDinner.Api.Filters;
+//using BuberDinner.Api.Middleware;
 using BuberDinner.Application;
 
 var builder = WebApplication.CreateBuilder(args);
 {
     builder.Services
         .AddAplication()
-        .AddInfrastructure(builder.Configuration);
-    builder.Services.AddControllers();
+        .AddInfrastructure(builder.Configuration)
+        //.AddControllers(options => options.Filters.Add<ErrorHandlingFilterAttribute>());
+        .AddControllers();
 }
 
 var app = builder.Build();
 {
+    //app.UseMiddleware<ErrorHandlingMiddleware>();
+    app.UseExceptionHandler("/error");
     app.UseHttpsRedirection();
     app.MapControllers();
     app.Run();
