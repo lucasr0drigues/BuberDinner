b7d148fdce74585614016705ed43e4f74e92f1a4
lucasr0drigues <lucas.a.rodrigues@outlook.com>
2022-07-25 16:42:13 -0300

    lesson 7 - add mapster

--- BuberDinner.Api/Program.cs
+++ BuberDinner.Api/Program.cs
@@ -1,18 +1,15 @@
 
-using BuberDinner.Api.Common.Errors;
-using BuberDinner.Api.Filters;
+using BuberDinner.Api;
 //using BuberDinner.Api.Middleware;
 using BuberDinner.Application;
-using Microsoft.AspNetCore.Mvc.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 {
     builder.Services
+        .AddPresentation()
         .AddAplication()
-        .AddInfrastructure(builder.Configuration)
-        //.AddControllers(options => options.Filters.Add<ErrorHandlingFilterAttribute>());
-        .AddSingleton<ProblemDetailsFactory, BuberDinnerProblemDetailsFactory>()
-        .AddControllers();
+        .AddInfrastructure(builder.Configuration);
+    //.AddControllers(options => options.Filters.Add<ErrorHandlingFilterAttribute>());
 }
 
 var app = builder.Build();
